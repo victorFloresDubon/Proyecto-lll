f8ab5ba95be5d337e435b0f968e088cc256653af
victorFloresDubon <vdubon@outlook.com>
2018-05-13 18:17:17 -0600

    Initial commit

---
+++ src/gt/umg/programacion/proyecto3/Tabla.java
@@ -0,0 +1,25 @@
+package gt.umg.programacion.proyecto3;
+
+public class Tabla {
+	private String nombre;
+	private String campo;
+	private String tipoDato;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCampo() {
+		return campo;
+	}
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+	public String getTipoDato() {
+		return tipoDato;
+	}
+	public void setTipoDato(String tipoDato) {
+		this.tipoDato = tipoDato;
+	}
+}
